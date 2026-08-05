55840257ce109443015d4a268b56a8c28dd55054
Saranyu-Ongkaew <saranyuo67@nu.ac.th>
2026-08-05 12:02:37 +0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// แบบฝึกหัดที่ 4
+// แบบฝึกหัดที่ 5
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -16,25 +16,45 @@ class MyApp extends StatelessWidget {
         ),
         body: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
+          child: Row(
             // คอลัมน์หลักสำหรับหน้า
             children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: Container(color: Colors.red, height: 100),
+              ),
+              Expanded(
+                flex: 2,
+                child: Container(color: Colors.green, height: 100),
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(color: Colors.blue, height: 100),
+              ),
+
               const Icon(Icons.account_circle, size: 50, color: Colors.indigo),
               const SizedBox(width: 16), // เว้นวรรคระหว่างไอคอนและข้อความ
-              Column(
-                // Column สำหรับรายละเอียดข้อความ
-                crossAxisAlignment:
-                    CrossAxisAlignment.start, // จัดข้อความชิดซ้าย
-                mainAxisSize:
-                    MainAxisSize.min, // ป้องกันไม่ให้ column ใช้ความสูงเต็ม
-                children: const <Widget>[
-                  Text(
-                    'โปรไฟล์ผู้ใช้',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 4), // เว้นวรรคระหว่าง title และ description
-                  Text('แก้ไขรายละเอียดโปรไฟล์ของคุณที่นี่'),
-                ],
+              Expanded(
+                child: Column(
+                  // Column สำหรับรายละเอียดข้อความ
+                  crossAxisAlignment:
+                      CrossAxisAlignment.start, // จัดข้อความชิดซ้าย
+                  mainAxisSize:
+                      MainAxisSize.min, // ป้องกันไม่ให้ column ใช้ความสูงเต็ม
+                  children: const <Widget>[
+                    Text(
+                      'โปรไฟล์ผู้ใช้',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 4,
+                    ), // เว้นวรรคระหว่าง title และ description
+                    Text('แก้ไขรายละเอียดโปรไฟล์ของคุณที่นี่'),
+                  ],
+                ),
               ),
             ],
           ),
